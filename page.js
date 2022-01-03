60ccd1775b9e38ff0575f67029234cbb79010505
Samira Silva <iasamirasilva@gmail.com>
2022-01-02 22:06:16 -0300

    Initial commit

---
+++ page.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Amarelo";
+    }
+    else {
+        this.textContent = "Azul";
+    }
+
+    console.log('current class name: ' + className);
+
+});
